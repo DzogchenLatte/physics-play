f9a336682a7fa8082745dfa66248ffd270924549
Andrew Decker <ajdecker1022@gmail.com>
2015-11-02 18:21:47 -0600

    Construct wall, make boxes into circles and reduce circle size

--- Sample.js
+++ Sample.js
@@ -6,13 +6,25 @@ var Engine = Matter.Engine,
 // create a Matter.js engine
 var engine = Engine.create(document.body);
 
+var boxWidth = 800;
+var boxHeight = 600;
+var wallThickness = 40;
+
 // create two boxes and a ground
-var boxA = Bodies.circle(400, 200, 80);
-var boxB = Bodies.circle(450, 50, 80);
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+var circleA = Bodies.circle(400, 200, 20);
+var circleB = Bodies.circle(450, 50, 20);
+
+var leftWall = Bodies.rectangle(wallThickness, boxHeight / 2, wallThickness, 
+	boxHeight, { isStatic: true });
+var rightWall = Bodies.rectangle(boxWidth - wallThickness, boxHeight / 2 , wallThickness, 
+	boxHeight, { isStatic: true });
+var topWall = Bodies.rectangle(boxWidth / 2, wallThickness, boxWidth, 
+	wallThickness, { isStatic: true });
+var bottomWall = Bodies.rectangle(boxWidth / 2, boxHeight - wallThickness, boxWidth,
+	wallThickness, { isStatic: true });
 
 // add all of the bodies to the world
-World.add(engine.world, [boxA, boxB, ground]);
+World.add(engine.world, [circleA, circleB, leftWall, rightWall, topWall, bottomWall]);
 
 // run the engine
 Engine.run(engine);
